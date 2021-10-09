43ae12417cdb25c853323ae312008036d2bd34f6
Anastassia <nastiatsv@gmail.com>
2021-10-09 12:37:14 -0700

    adding files from javascripting exercises

---
+++ accessing-array-values.js
@@ -0,0 +1,20 @@
+/*
+Valid notation:  
+   
+     console.log(pets[0])  
+   
+  Invalid notation:  
+   
+     console.log(pets.1);  
+   
+ ## The challenge:  
+   
+  Create a file named accessing-array-values.js.  
+   
+  In that file, define array food :  
+   
+     const food = ['apple', 'pizza', 'pear']  */
+
+
+const food = ['apple', 'pizza', 'pear']
+console.log(food[1])
---
+++ string-length.js
@@ -0,0 +1,4 @@
+//Assign the string 'example string' to the variable example.  
+
+const example = 'example string'
+console.log(example.length);
